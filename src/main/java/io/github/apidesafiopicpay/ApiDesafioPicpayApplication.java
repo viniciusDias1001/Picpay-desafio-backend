625f434d32bbefd3596bf53f0278fad5453e63bb
Vinicius Dias <105192228+viniciusDias1001@users.noreply.github.com>
2023-08-25 14:52:16 -0300

    fist commit

---
+++ src/main/java/io/github/apidesafiopicpay/ApiDesafioPicpayApplication.java
@@ -0,0 +1,13 @@
+package io.github.apidesafiopicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioPicpayApplication.class, args);
+	}
+
+}
