d3fb004dfd9b8ef2a68bff7301d36fcf201337fe
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-08-25 16:13:36 -0300

    entity user mapeada

--- src/main/java/io/github/apidesafiopicpay/entity/User.java
+++ src/main/java/io/github/apidesafiopicpay/entity/User.java
@@ -1,6 +1,77 @@
 package io.github.apidesafiopicpay.entity;
 
-import io.github.apidesafiopicpay.security.UserSecurity;
+import io.github.apidesafiopicpay.enums.Document;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User extends UserSecurity {
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails, Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    @NotBlank(message = "Email can't be null/blank")
+    private String email;
+    @NotBlank(message = "Nome Completo can't be null/blank")
+    private String nomeCompleto;
+    @NotBlank(message = "password can't be null/blank")
+    private String password;
+    @NotBlank(message = "Document can't be null/blank")
+    @CPF
+    @CNPJ
+    private Document document;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
