71b02a14e316b8ef3c01322fb5a65a24484a3a82
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-08-25 18:00:31 -0300

    migration forçadamente lida

--- src/main/java/io/github/apidesafiopicpay/config/TestConfig.java
+++ src/main/java/io/github/apidesafiopicpay/config/TestConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
 public class TestConfig implements CommandLineRunner {
 
     @Autowired
