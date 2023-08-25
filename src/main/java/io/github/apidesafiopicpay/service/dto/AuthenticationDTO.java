8083087480eb81f9dbe5b46d0b4ccf5673b22df8
Anderson Alexsandro Soares <anderson.silva.04@outlook.com>
2023-08-25 19:34:51 -0300

    Dto agora são record

--- src/main/java/io/github/apidesafiopicpay/service/dto/AuthenticationDTO.java
+++ src/main/java/io/github/apidesafiopicpay/service/dto/AuthenticationDTO.java
@@ -1,32 +1,6 @@
 package io.github.apidesafiopicpay.service.dto;
 
-public class AuthenticationDTO {
-
-    private String email;
-    private String password;
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String login) {
-        this.email = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public AuthenticationDTO() {
-    }
-
-    public AuthenticationDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
+public record AuthenticationDTO(
+        String email,
+        String password) {
 }
